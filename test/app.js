340c2c6164ebc1b90695b5d68960df7472677b7f
Salim KAYABAŞI <salim.kayabasi@gmail.com>
2016-06-08 11:59:32 +0300

    fixing tests and adding `kapgel-logger` to template project

--- test/app.js
+++ test/app.js
@@ -3,16 +3,30 @@ var path = require('path');
 var assert = require('yeoman-assert');
 var helpers = require('yeoman-test');
 
-describe('generator-kapgel-base:app', function () {
+describe('generator-kapgel-base:app with default params', function () {
   before(function () {
     return helpers.run(path.join(__dirname, '../generators/app'))
-      .withPrompts({someAnswer: true})
       .toPromise();
+  });
+
+  it('creates folders', function () {
+    assert.directory([
+      'lib',
+      'test'
+    ]);
   });
 
   it('creates files', function () {
     assert.file([
-      'dummyfile.txt'
+      '.editorconfig',
+      '.eslintrc',
+      '.gitignore',
+      '.npmignore',
+      '.travis.yml',
+      'index.js',
+      'readme.md',
+      'test/test.js',
+      'lib/index.js'
     ]);
   });
 });
